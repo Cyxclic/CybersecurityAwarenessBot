761d54b7a8edb0b42062daa99f94ee27baae687a
Cyxclic <st10474412@rcconnect.edu.za>
2026-04-13 13:33:50 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -66,7 +66,7 @@ namespace CybersecurityAwarenessBot
                 Console.WriteLine("Error playing greeting: " + ex.Message);
             }
         }
-
+// minor improvement
         static void DisplayLogo()
         {
             Console.ForegroundColor = ConsoleColor.Green;
